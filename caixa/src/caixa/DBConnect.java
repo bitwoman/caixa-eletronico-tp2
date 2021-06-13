593aaf3ccf84c4bb2028a4c46d89b1954189d200
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2021-06-13 15:12:41 -0300

    add DB with errors

--- caixa/src/caixa/DBConnect.java
+++ caixa/src/caixa/DBConnect.java
@@ -1,27 +1,40 @@
 package caixa;
 
 
-import java.sql.*;  
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mysql.jdbc.Connection;  
 
 public class DBConnect {
-	public void connect() {
+
+
+	public Connection connect() {
 		try{  
 			Class.forName("com.mysql.jdbc.Driver");  
 			Connection con=DriverManager.getConnection(  
 				"jdbc:mysql://localhost:3306/caixa","root","123");  
-
 			
-			Statement state=con.createStatement();  
-			//test
-			ResultSet result=state.executeQuery("select * from test");
-			
-			while(result.next()) {  
-				System.out.println(result.getInt(1));  
-			}
-			con.close(); 
-
+			return con;
 		}catch(Exception e){
 			System.out.println("Error: "+ e);
-		}  
+			return null;
+
+		}
+	}
+	
+	
+	public void insertDB(String table) throws SQLException {	
+		//ResultSet result=this.connect()state.executeQuery("insert into "+ table + " values (id)" + 1 + ";");
+		
+		String value = "insert into " + table +" (id) values(88);";
+		System.out.printf(value);
+		var insert = this.connect().prepareStatement(value);
+		insert.executeUpdate();
+		     
+	}
+	public void close() {
+		con.close();
 	}
 }
--- caixa/src/caixa/Main.java
+++ caixa/src/caixa/Main.java
@@ -3,22 +3,9 @@ import java.sql.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try{  
-			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con=DriverManager.getConnection(  
-					"jdbc:mysql://localhost:3306/caixa","root","123");  
-
-			Statement stmt=con.createStatement();  
-			ResultSet rs=stmt.executeQuery("select * from test");  
-			while(rs.next()) {  
-				System.out.println(rs.getInt(1));  
-			}
-			con.close(); 
-
-		}catch(Exception e){
-			System.out.println(e);
-		}  
+	public static void main(String[] args) throws SQLException {
+		DBConnect db = new DBConnect();
+		db.insertDB("test");
 		
 	}
 
