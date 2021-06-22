a15cc026fd24a9d5ad9b4cbf64e48130058956f1
Brenda <49522696+bitwoman@users.noreply.github.com>
2021-06-22 19:40:28 -0300

    Update create-database-caixa.sql

--- banco de dados/create-database-caixa.sql
+++ banco de dados/create-database-caixa.sql
@@ -31,7 +31,9 @@
 		tipoConta VARCHAR(50) NOT NULL,
 		saldoConta DOUBLE(10, 2) NOT NULL,
 		statusConta INT NOT NULL,
-		CONSTRAINT fk_idConta PRIMARY KEY (idConta)
+        idCliente INT NOT NULL,
+		CONSTRAINT pk_idConta PRIMARY KEY (idConta),
+        CONSTRAINT fk_idCliente FOREIGN KEY (idCliente) REFERENCES cliente (idCliente)
 	); 
  
  /*
@@ -50,8 +52,30 @@
 		senha CHAR(6) NOT NULL,
 		CONSTRAINT pk_idCliente PRIMARY KEY (idCliente)
 	);
+    
+/*
+ * tipoOperacoes: 1 - depósito, 2 - saque, 3 - extrato, 4 - consulto do usuário
+ */
+    DROP TABLE IF EXISTS tipoOperacoes;
+    CREATE TABLE tipoOperacoes (
+		idTipoOperacao INT NOT NULL AUTO_INCREMENT,
+        tipoOperacao VARCHAR(50) NOT NULL,
+		CONSTRAINT pk_idTipoOperacao PRIMARY KEY (idTipoOperacao)
+    );
 
-DESCRIBE cliente;
+/*
+ *  operações
+ */
+    DROP TABLE IF EXISTS operacoes;
+    CREATE TABLE operacoes (
+		idOperacao INT NOT NULL AUTO_INCREMENT,
+        dataOperacao DATETIME NOT NULL,
+		idTipoOperacao INT NOT NULL,
+        valorOperacao DOUBLE (10,2) NOT NULL,
+        CONSTRAINT pk_idOperacao PRIMARY KEY (idOperacao),
+        CONSTRAINT fk_idTipoOperacao FOREIGN KEY (idTipoOperacao) REFERENCES tipoOperacoes (idTipoOperacao)
+    );
+
 /*
  * INSERÇÃO DE DADOS
  */
@@ -59,7 +83,7 @@ DESCRIBE cliente;
 /*
  * ADD agencia
  */
-	INSERT INTO agencia (nomeAgencia, numAgencia) VALUES ('Agência', 1234);
+    INSERT INTO agencia (nomeAgencia, numAgencia) VALUES ('Agência', 1234);
     INSERT INTO agencia (nomeAgencia, numAgencia) VALUES ('Agência', 5678);
     INSERT INTO agencia (nomeAgencia, numAgencia) VALUES ('Agência', 9101);
     INSERT INTO agencia (nomeAgencia, numAgencia) VALUES ('Agência', 1213);
@@ -75,8 +99,11 @@ DESCRIBE cliente;
  */
  
  /*
- * ADD cliente
- */
-	INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Brenda Leite', 'Rua da Glória, 21', 3333-3333, 5000.00, 19018025, 'Agência', 01, '123456'); 
-	INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Marcos Vicente', 'Rua da Vitória, 30', 4444-4444, 5000.00, 20018025, 'Agência', 02, '654321');
+  * ADD cliente
+  */
+    INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Brenda Leite', 'Rua da Glória, 21', 3333-3333, 5000.00, 19018025, 'Agência', 01, '123456'); 
+    INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Marcos Vicente', 'Rua da Vitória, 30', 4444-4444, 5000.00, 20018025, 'Agência', 02, '654321');
+    INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Maria Cristina', 'Rua José Constantino, 94', 5555-5555, 1500.00, 20029026, 'Agência', 03, '789101');
+    INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Marcos Vicente', 'Rua da Vitória, 30', 4444-4444, 3500.00, 20030027, 'Agência', 04, '111213');
+    INSERT INTO cliente (nomeCliente, enderecoCliente, telefoneCliente, rendaCliente, cepCliente, nomeAgencia, numeroConta, senha) VALUES ('Marcos Vicente', 'Rua da Vitória, 30', 4444-4444, 4500.00, 20040028, 'Agência', 05, '141516');
     
